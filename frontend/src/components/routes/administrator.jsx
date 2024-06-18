fe21a6aa889cd0c51167a566f9f209ed4d689eb6
David <david.akram@corporatica.com>
2024-06-18 20:07:45 +0300

    v2 updates

--- frontend/src/components/routes/administrator.jsx
+++ frontend/src/components/routes/administrator.jsx
@@ -4,17 +4,9 @@ import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
 
 const PrivateRouteWithUserCheck = ({ component: Component, ...rest }) => {
-  const [user, setUser] = useState("");
   const token = Cookies.get("zodiac_token");
 
-  useEffect(() => {
-    if (token) {
-      const { user_name } = jwtDecode(token);
-      setUser(user_name);
-    }
-  }, [token]);
-
-  if (token && user === "Youssef") {
+  if (token && jwtDecode(token).user_name === "Youssef") {
     return <Route {...rest} render={(props) => <Component {...props} />} />;
   }
 
